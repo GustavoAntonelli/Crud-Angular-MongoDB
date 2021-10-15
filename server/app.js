f6dd28af0bec2c2d434002cefe5479a0befd19c3
GustavoAntonelli <gustavo.antonelli.souza@hotmail.com>
2021-10-15 08:52:10 -0300

    first commit

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const department_controller = require('./department_controller');
+const product_controller = require('./product_controller');
+
+const app = express();
+const port = 3000;
+const db = "mongodb+srv://gustavo_antonelli:gustavo_antonelli@cluster0.pdhkp.mongodb.net/aplicacao_httpp?retryWrites=true&w=majority"
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, (err, sucess)=>{
+    if (err) {
+        console.log(`Falha na conexão ao BD`);
+    }
+    console.log(`Banco de dados conectado`);
+})
+
+app.use('/departments', department_controller);
+app.use('/products', product_controller)
+
+
+app.listen(port, ()=>{
+    console.log(`servidor started on port ${port}`);
+    
+})
+
+
+
